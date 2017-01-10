5192c1290dcf2ca592a66053e5c1974b9086a604
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-01-10 18:35:37 -0500

    added meters to miles converter

---
+++ AC3.2-Cheers/CellDisplay&Functionality.swift
@@ -0,0 +1,9 @@
+//
+//  CellDisplay&Functionality.swift
+//  AC3.2-Cheers
+//
+//  Created by Marty Avedon on 1/10/17.
+//  Copyright © 2017 Marty Hernandez Avedon. All rights reserved.
+//
+
+import Foundation
