01da5b27ee7e4bb771650836d9afd45b5d5dfe6f
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-01-10 11:01:53 -0500

    added cell files (.swift, .nib) & added image assets in 3 colors + heart, bottle cap, light & dark beer + gold, gray, black + heart icon can be colored from code -- other icons are not!

---
+++ AC3.2-Cheers/AC3.2-Cheers/BasicCheersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BasicCheersTableViewCell.swift
+//  AC3.2-Cheers
+//
+//  Created by Marty Avedon on 1/10/17.
+//  Copyright © 2017 Marty Hernandez Avedon. All rights reserved.
+//
+
+import UIKit
+
+class BasicCheersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
