5b0595aceda4d1d4f295d1a5c38445b97bdf61dc
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-01-13 22:47:06 -0500

    Initial Commit

---
+++ Pods/Target Support Files/SKSplashView/SKSplashView-umbrella.h
@@ -0,0 +1,10 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "SKSplashIcon.h"
+#import "SKSplashView.h"
+
+FOUNDATION_EXPORT double SKSplashViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char SKSplashViewVersionString[];
+
